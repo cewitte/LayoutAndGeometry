44402d81e973ba9cf1b62eb1498421a43671c998
Carlos Eduardo Witte <cewitte@icloud.com>
2025-08-13 15:24:33 -0300

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Carlos Eduardo Witte on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
